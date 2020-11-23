2441c37186cbdf748162717e9dc5d2662ca7990e
Iron-Cow <uangeji@gmail.com>
2020-11-23 22:06:35 +0200

    media files done

--- forum/views.py
+++ forum/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
+from .models import Post
+from django.http import Http404
 
 
 def post_list(request):
-    return render(request, 'forum/post_list.html')
+    data = {}
+    all_posts = Post.objects.all()
+    data['posts'] = all_posts
+    return render(request, 'forum/post_list.html', context=data)
 
 
 def post_details(request, post_id: int):
-    return render(request, 'forum/post_details.html')
+    if not Post.objects.filter(id=post_id).exists():
+        raise Http404
+    post = Post.objects.get(id=post_id)
+    data = dict()
+    data['post'] = post
+    return render(request, 'forum/post_details.html', context=data )
