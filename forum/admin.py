882401b954e6f13ef4ec7399f8140249f46ae740
Iron-Cow <uangeji@gmail.com>
2020-11-30 21:30:36 +0200

    search and admin tool implemented

--- forum/admin.py
+++ forum/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from .models import Post
 
-# Register your models here.
-admin.site.register(Post)
+
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'timestamp', 'updated', 'draft']
+    list_display_links = ['author', 'timestamp']
+    list_editable = ['title']
+
+    list_filter = ['title', 'author', 'timestamp', 'updated']
+    search_fields = ['author__username', 'title']
+
+    class Meta:
+        model = Post
+
+admin.site.register(Post, PostModelAdmin)
--- forum/views.py
+++ forum/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Post
 from django.http import Http404, JsonResponse
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 
 def post_list(request):
@@ -14,12 +15,36 @@ def post_list(request):
     # last_post.save()
     # # end to delete
 
+    search = request.GET.get('search')
+    print(search)
+    if search:
+
+        # слишком просто
+        # result_posts = all_posts.filter(title__icontains=search)
+        # all_posts = result_posts
+
+        # круто
+        result_posts = all_posts.filter(
+            Q(title__icontains=search) |
+            Q(author__username__icontains=search)
+            # & Q(author__is_superuser=True)
+        )
+        all_posts = result_posts
+        data['search'] = search
+
+
+
     paginator = Paginator(all_posts, 6)
     page_number = request.GET.get('page')
+
+
+    # if int(page_number) > paginator.num_pages:
+    #     page_number = paginator.num_pages
     page_obj = paginator.get_page(page_number)
 
     # data['posts'] = all_posts
     data['page_obj'] = page_obj
+
 
     return render(request, 'forum/post_list.html', context=data)
 
@@ -30,8 +55,12 @@ def post_details(request, post_id: int):
     post = Post.objects.get(id=post_id)
     data = dict()
     data['post'] = post
+
     # fixme
-    # data['liked'] = post.likes.filter(request.user)
+    data['liked'] = request.user in post.likes.all()
+    print(post.likes.all())
+
+
     return render(request, 'forum/post_details.html', context=data )
 
 
